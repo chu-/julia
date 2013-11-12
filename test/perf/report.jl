31d1fa43978eb456125767d54e4bda9347865ac1
Elliot Saba <staticfloat@gmail.com>
2013-11-11 19:10:02 -0800

    Move to status.julialang.org

--- test/perf/report.jl
+++ test/perf/report.jl
@@ -4,4 +4,4 @@ env_name = chomp(readall(`hostname`))
 commit = Base.BUILD_INFO.commit
 flavor = ENV["JULIA_FLAVOR"]
 json = "{\"env\": \"$env_name\", \"blas\":\"$flavor\", \"commit\":\"$commit\"}"
-post("http://julia-codespeed.csail.mit.edu:8000/put/codespeed", json ) 
+post("http://status.julialang.org/put/codespeed", json )
